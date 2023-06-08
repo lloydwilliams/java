2fcbf2802a1323c9e2c04b3c6cdb7987d28705ae
lloydwilliams <lloyd.williams@datadoghq.com>
2023-06-08 18:26:23 -0400

    updates for connection pooling
    updates for connection pooling - c3p0 and hikariCP

--- payroll/eclipse/workspace11/payroll/run-payroll.sh
+++ payroll/eclipse/workspace11/payroll/run-payroll.sh
@@ -8,6 +8,7 @@ export JAVA_HOME=/Library/Java/JavaVirtualMachines/openjdk-11.jdk/Contents/Home
 
 # -Duser.timezone=UTC
 
-$JAVA_HOME/bin/java -javaagent:/Users/lloyd.williams/u01/datadog/dd-java-agent.jar \
- -Ddd.trace.methods=com.example.payroll.EmployeeController[*] \
- -Ddd.logs.injection=true -Ddd.service=payroll -Ddd.source=java -Ddd.env=dev -Ddd.version=11.0.1 -jar target/payroll-11.0.1.jar
+$JAVA_HOME/bin/java -javaagent:/Users/lloyd.williams/u01/datadog/dd-java-agent.jar -XX:FlightRecorderOptions=stackdepth=256 \
+ -Ddd.profiling.enabled=true -Ddd.trace.methods=com.example.payroll.EmployeeController[*] \
+ -Ddd.appsec.enabled=true \
+ -Ddd.logs.injection=true -Ddd.service=payroll -Ddd.source=payroll -Ddd.env=dev -Ddd.version=11.0.2 -jar target/payroll-11.0.2.jar
